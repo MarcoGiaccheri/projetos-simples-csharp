39c90b1c094c41a010e07d63a456cf2bae4717a6
MarcoGiaccheri <giaccheri@outlook.com>
2022-04-29 22:00:30 -0300

    fix: Calculadora IMC

--- CalculadoraIMC/Program.cs
+++ CalculadoraIMC/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double peso = 0;
+double altura = 0;
+
+Console.WriteLine("Digite seu peso (kg)");
+peso = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite sua altura (metros)");
+altura = double.Parse(Console.ReadLine());
+
+double imc = (peso / (altura * altura));
+
+Console.WriteLine($"Se você pesa {peso} Kg e mede {altura.ToString("N2")} metros de altura seu imc é {imc.ToString("N1")}");
