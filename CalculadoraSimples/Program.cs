42e1b20bb8f3452cafc8587729e0c4d309f0686b
MarcoGiaccheri <giaccheri@outlook.com>
2022-05-04 11:55:57 -0300

    feat: Calculadora simples

---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,32 @@
+﻿double primeiroNumero = 0;
+double segundoNumero = 0;
+double resultado = 0;
+char tipoOperaçao;
+
+Console.WriteLine("Digite o primeiro número: ");
+primeiroNumero = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+segundoNumero = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o tipo de operação desejada (+, -, *, /): ");
+tipoOperaçao = char.Parse(Console.ReadLine());
+
+if (tipoOperaçao == '+')
+{
+    resultado = primeiroNumero + segundoNumero;
+}
+else if (tipoOperaçao == '-')
+{
+    resultado = primeiroNumero - segundoNumero;
+}
+else if (tipoOperaçao == '*')
+{
+    resultado = primeiroNumero * segundoNumero;
+}
+else if (tipoOperaçao == '/')
+{
+    resultado = primeiroNumero / segundoNumero;
+}
+
+Console.WriteLine("O resultado é:" + resultado);
