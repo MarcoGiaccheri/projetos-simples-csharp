613e07cdd35e83c4d3785c60268833a4b9cfc23a
MarcoGiaccheri <giaccheri@outlook.com>
2022-05-05 21:18:55 -0300

    feat: MediaAritmetica

---
+++ MediaAritmetica/Program.cs
@@ -0,0 +1,20 @@
+﻿double primeiraNota = 0;
+double segundaNota = 0;
+double terceiraNota = 0;
+double quartaNota = 0;
+
+Console.WriteLine("Digite a primeira nota: ");
+primeiraNota = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a segunda nota: ");
+segundaNota = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a terceira nota: ");
+terceiraNota = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a quarta nota: ");
+quartaNota = double.Parse(Console.ReadLine());
+
+double mediaAritmetica = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4;
+
+Console.WriteLine("Sua média final é " + mediaAritmetica.ToString("N1"));
