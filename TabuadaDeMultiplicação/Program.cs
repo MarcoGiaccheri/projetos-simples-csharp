3b4970f324f5213003aeba8e5a8c6cd1b3c5448d
MarcoGiaccheri <giaccheri@outlook.com>
2022-05-17 18:13:25 -0300

    feat: TabuadaDeMultiplicação

---
+++ TabuadaDeMultiplicação/Program.cs
@@ -0,0 +1,22 @@
+﻿
+double numero;
+Console.Write("Digite o número da tabuada: ");
+numero = double.Parse(Console.ReadLine());
+
+if (numero == 0)
+{
+    Console.WriteLine("Número inválido");
+}
+else
+{
+    int parametro;
+    Console.Write("Digite um número inteiro indicando até onde vai rodar a sua tabuada: ");
+    parametro = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i <= parametro; i++)
+    {
+        double total = numero * i;
+        Console.WriteLine(numero + " X " + i + " = " + total);
+
+    }
+}
